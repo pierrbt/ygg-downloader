9287b369192e91c4d4e9a501d66271c18ff11d61
pierrbt <pierre08bt@gmx.co.uk>
2023-11-04 23:26:53 +0100

    feat: stats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App(props: AppProps) {
           content={metadata.description}
           key="ogdesc"
         />
-        <script defer data-domain="ygg.veagle.fr" src="https://stats.veagle.fr/js/script.js"></script>
+        <script defer data-domain="ygg.veagle.fr" src="https://acabou.veagle.fr/js/script.js"></script>
 
       </Head>
 
